7deeb0c25dfc421c49bc9644a4d08937642478cd
jiyanchen <orzz7.328@gmail.com>
2023-02-07 14:42:41 +0800

    1

---
+++ vue text/dist/js/List_Rendering.js
@@ -0,0 +1,23 @@
+let id = 0
+
+Vue.createApp({
+  data() {
+    return {
+      newTodo: '',
+      todos: [
+        { id: id++, text: 'Learn HTML' },
+        { id: id++, text: 'Learn JavaScript' },
+        { id: id++, text: 'Learn Vue' }
+      ]
+    }
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({ id: id++, text: this.newTodo })
+      this.newTodo = ''
+    },
+    removeTodo(todo) {
+      this.todos = this.todos.filter((t) => t !== todo)
+    }
+  }
+}).mount('#app')
